95dd4ecaa006a0e8aeed32ffc95d5d3e74b796a8
Alex Chen <alexschen94@gmail.com>
2015-07-21 00:00:07 -0500

    Add integration testing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,11 +10,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "gets help" do
     get :help
     assert_response :success
-    assert_select 'title', 'Help | Ruby on Rails Tutorial Sample App'  end
+    assert_select 'title', 'Help | Ruby on Rails Tutorial Sample App'  
+  end
 
   test 'gets about' do
     get :about
     assert_response :success
     assert_select 'title', 'About | Ruby on Rails Tutorial Sample App'
+  end
+
+  test 'gets contact' do
+    get :contact
+    assert_response :success
+    assert_select 'title', 'Contact | Ruby on Rails Tutorial Sample App'
   end
 end
